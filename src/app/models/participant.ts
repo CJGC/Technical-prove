40a3ee29a7a6f6a86226189dea73ce61059b9d24
cjgc <chjugomez@utp.edu.co>
2019-07-18 09:00:17 -0500

    Files were deleted

--- src/app/models/participant.ts
+++
@@ -1,9 +0,0 @@
-export class Participant {
-    constructor (
-        public participant_id : number,
-        public name : string,
-        public surname : string,
-        public email : string,
-    ) {}
-
-}
