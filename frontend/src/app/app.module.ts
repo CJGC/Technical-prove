8b6696de761d241c88af7a652940320a25bc98f3
cjgc <chjugomez@utp.edu.co>
2019-07-19 16:47:27 -0500

    adding dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActCreateComponent } from './components/act/act-create.component';
 import { ActListComponent } from './components/act/act-list.component';
 import { ActShowComponent } from './components/act/act-show.component';
 import { MissinWebPageComponent } from './components/errors/missing-webpage.component';
@@ -17,9 +16,7 @@ import { ParticipantShowComponent } from './components/participant/participant-s
 import { ParticipantDeleteComponent } from './components/participant/participant-delete.component';
 import { ParticipantEditComponent } from './components/participant/participant-edit.component';
 import { ActParticipantsComponent } from './components/actParticipants/act-participants.component';
-import { PickListModule } from 'primeng/picklist';
-import { DataViewModule } from 'primeng/dataview';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GetParticipantsFromActaComponent } from './components/commitment/get-participants-from-acta.component';
 import { CreateCommitmentComponent } from './components/commitment/commitment.create.component';
 import { GeneralProvider } from './providers/general.provider';
@@ -27,13 +24,22 @@ import { ParticipantCommitmentsComponent } from './components/commitment/partici
 import { CommitmentShowComponent } from './components/commitment/commitment-show.component';
 import { CommitmentEditComponent } from './components/commitment/component-edit.component';
 import { CommitmentDeleteComponent } from './components/commitment/commitment-delete.component';
+import {CalendarModule} from 'primeng/calendar';
+
+import { DialogService, MessageService } from 'primeng/api';
+import { DynamicDialogModule } from 'primeng/components/dynamicdialog/dynamicdialog';
+import { CreateActComponent } from './components/act/act-create.component';
+
+import { PickListModule } from 'primeng/picklist';
+import { DataViewModule } from 'primeng/dataview';
+import {ToastModule} from 'primeng/toast';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     ActListComponent,
-    ActCreateComponent,
+    CreateActComponent,
     ActShowComponent,
     ActEditComponent,
     ActDeleteComponent,
@@ -52,6 +58,7 @@ import { CommitmentDeleteComponent } from './components/commitment/commitment-de
     CommitmentEditComponent,
     CommitmentDeleteComponent,
     ParticipantCommitmentsComponent,
+
     MissinWebPageComponent
   ],
   imports: [
@@ -61,9 +68,14 @@ import { CommitmentDeleteComponent } from './components/commitment/commitment-de
     HttpClientModule,
     PickListModule,
     DataViewModule,
-    FormsModule
+    ToastModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    DynamicDialogModule
   ],
-  providers: [GeneralProvider],
+  entryComponents : [CreateActComponent],
+  providers: [GeneralProvider, DialogService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
