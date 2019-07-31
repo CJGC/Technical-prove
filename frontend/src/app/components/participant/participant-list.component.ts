d9d5a2d120da14e9ef2c2d78dc26bdc85bc74cad
cjgc <chjugomez@utp.edu.co>
2019-07-31 11:30:54 -0500

    Added dialog service

--- frontend/src/app/components/participant/participant-list.component.ts
+++ frontend/src/app/components/participant/participant-list.component.ts
@@ -3,11 +3,15 @@ import { ParticipantService } from 'src/app/services/participant.service';
 import { Participant } from 'src/app/models/participant';
 import { GeneralProvider } from 'src/app/providers/general.provider';
 import { Router } from '@angular/router';
+import { DialogService, MessageService } from 'primeng/api';
+import { ParticipantCreateComponent } from './participant-create.component';
+import { ParticipantEditComponent } from './participant-edit.component';
+import { ParticipantDeleteComponent } from './participant-delete.component';
 
 @Component ({
     selector : 'participant-list',
     templateUrl : '../../views/participant/participant-list.html',
-    providers : [ParticipantService]
+    providers : [ParticipantService, DialogService, MessageService]
 })
 export class ParticipantListComponent {
     private title : string;
@@ -17,13 +21,15 @@ export class ParticipantListComponent {
         private _route : Router,
         private generalProvider : GeneralProvider,
         private participantService : ParticipantService,
+        private dialogService : DialogService,
+        private messageService : MessageService
     )
     {
         this.title = "Participants management";
+        this.getParticipants();
     }
 
-    ngOnInit() {
-        let data : string;
+    public getParticipants() {
         this.participantService.getParticipantList().
         subscribe(
             part => this.participantList = part,
@@ -38,5 +44,30 @@ export class ParticipantListComponent {
         this.generalProvider.clearData();
         this.generalProvider.setData([participant]);
         this._route.navigate([url]);
-    } 
+    }
+
+    public openDialog(part : Participant, tc : string) {
+        let targetComponent : any;
+
+        if (tc == "create")
+            targetComponent = ParticipantCreateComponent;
+        else if(tc == "edit")
+            targetComponent = ParticipantEditComponent;
+        else
+            targetComponent = ParticipantDeleteComponent;
+
+        let dialog = this.dialogService.open(
+            targetComponent,
+            {
+                header : tc + ' part',
+                width : '60%',
+                data : {part : part}
+            }
+        );
+
+        dialog.onClose.subscribe (
+            res => {if(res != null) this.getParticipants()},
+            error => console.log("Error closing dialog")
+        );
+    }
 }
