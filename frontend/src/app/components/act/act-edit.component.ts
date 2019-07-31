a9418abb25b8356e91d62125c56cc06ad802b0be
cjgc <chjugomez@utp.edu.co>
2019-07-31 09:31:24 -0500

    Message service was added in providers

--- frontend/src/app/components/act/act-edit.component.ts
+++ frontend/src/app/components/act/act-edit.component.ts
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
     selector : 'act-edit',
     templateUrl : '../../views/act/act-edit.html',
-    providers : [ActService]
+    providers : [ActService, MessageService]
 })
 export class ActEditComponent {
     private title : string;
