f7ca5a32991aae55cc686585ce048d6ad2717038
cjgc <chjugomez@utp.edu.co>
2019-08-01 14:59:23 -0500

    implemented dynamic dialog

--- frontend/src/app/components/participant/participant-delete.component.ts
+++ frontend/src/app/components/participant/participant-delete.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ParticipantService } from 'src/app/services/participant.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { DynamicDialogRef, DynamicDialogConfig, MessageService } from 'primeng/api';
+import { Participant } from 'src/app/models/participant';
 
 @Component({
     selector : 'participant-del',
@@ -9,22 +10,38 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ParticipantDeleteComponent {
 
+
     constructor (
+        private dynamicDialogRef : DynamicDialogRef,
+        private dynamicDialogConf : DynamicDialogConfig,
+        private messageService : MessageService,
         private participantService : ParticipantService,
-        private activatedRoute : ActivatedRoute,
-        private _router : Router
     ) 
     { }
 
-    delParticipant() {
-        let id : number;
-        this.activatedRoute.params.subscribe(
-            params =>  id = parseInt(params['id'],10)
-        );
-        this.participantService.delParticipant(id).
+    public delParticipant() : void {
+        let participant : Participant = this.dynamicDialogConf.data;
+
+        this.participantService.delParticipant(participant.participant_id).
         subscribe(
-            response => this._router.navigate(["/participant-list"]),
+            response => {
+                this.dynamicDialogRef.close(participant);
+                this.messageService.add({
+                    severity : 'success',
+                    summary : 'Info',
+                    detail : 'Participant was deleted successfully'
+                })
+            },
             error => console.log("Error deleting participant: ", error)
         );
+    }
+
+    public cancel() : void  {
+        this.dynamicDialogRef.close();
+        this.messageService.add({
+            severity : 'info',
+            summary : 'Info',
+            detail : 'Participant was not deleted'
+        });
     }
 }
