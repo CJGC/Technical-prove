90285de5093d5fe6b2bd51c64232442f4f2c1978
cjgc <chjugomez@utp.edu.co>
2019-08-01 18:14:03 -0500

    Added dynamic dialog support

--- frontend/src/app/components/commitment/get-participants-from-acta.component.ts
+++ frontend/src/app/components/commitment/get-participants-from-acta.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { actParticipantsService } from 'src/app/services/act-participants.service';
 import { Participant } from 'src/app/models/participant';
-import { GeneralProvider } from 'src/app/providers/general.provider';
 import { ActService } from 'src/app/services/act.service';
 import { Act } from 'src/app/models/act';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { DialogService } from 'primeng/api';
+import { CreateCommitmentComponent } from './commitment.create.component';
 
 @Component({
     selector : 'get-part-from-acta',
     templateUrl : '../../views/commitment/part-from-act.html',
-    providers : [actParticipantsService, ActService]
+    providers : [actParticipantsService, ActService, DialogService]
 })
 export class GetParticipantsFromActaComponent {
 
@@ -19,30 +20,55 @@ export class GetParticipantsFromActaComponent {
     private subtitle : string;
 
     constructor(
-        private _router : Router,
+        private dialogService : DialogService,
+        private activatedRoute : ActivatedRoute,
         private actParticipantsService : actParticipantsService,
-        private generalProvider : GeneralProvider
+        private actProvider : ActService
     ) 
     {
         this.participants = [];
         this.title = "Select desired participant to add a commitment!";
-        this.act = <Act> this.generalProvider.getData().pop();
-        this.subtitle = "PROJECT'S ACT: " + this.act.project;
-        this.actParticipantsService.getActParticipantsByActId(this.act.actId).
-            subscribe (
-                actPart => actPart.forEach(
-                    ele => this.participants.push(ele.participants)
-                ),
-                error => console.log("Error getting actParticipants", error),
-            );
+
+        this.activatedRoute.paramMap.subscribe(param => {
+            const actId = +param.get("id");
+            if (actId == null) {
+                console.log("Incorrect argument's name");
+                return
+            }
+            this.actProvider.getAct(actId).subscribe (
+                res => this.act = res,
+                error => console.log("Error getting act: ", error),
+                () => this.getParticipants()
+            );                
+        });
     }
 
-    public saveIntoGeneralProvider(
-        url : string, 
-        participant : Participant
-    ) : void {
-        this.generalProvider.clearData();
-        this.generalProvider.setData([this.act, participant]);
-        this._router.navigate([url]);
+    public getParticipants() {
+        this.subtitle = "PROJECT'S ACT: " + this.act.project;
+        this.actParticipantsService.getActParticipantsByActId(this.act.actId).
+        subscribe (
+            actPart => actPart.forEach(
+                ele => this.participants.push(ele.participants)
+            ),
+            error => console.log("Error getting actParticipants", error),
+        );
+    }
+
+    public openDialog(part : Participant) {
+
+        let dialog = this.dialogService.open(
+            CreateCommitmentComponent,
+            {
+                header : ' Create commitment',
+                width : '60%',
+                data : {act : this.act, part : part}
+            }
+        );
+
+        dialog.onClose.subscribe( 
+            response => { },
+            error => console.log("Error closing dialog", error)
+        );
+    
     }
 }
