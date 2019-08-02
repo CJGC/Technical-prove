3f37ba206663ea7a3d9ae0ec1701cee6ab854044
cjgc <chjugomez@utp.edu.co>
2019-08-01 19:09:42 -0500

    this was adjusted for load commitments from query

--- frontend/src/app/components/commitment/participant-commitments.component.ts
+++ frontend/src/app/components/commitment/participant-commitments.component.ts
@@ -3,42 +3,53 @@ import { CommitmentService } from 'src/app/services/commitment.service';
 import { GeneralProvider } from 'src/app/providers/general.provider';
 import { Participant } from 'src/app/models/participant';
 import { Commitment } from 'src/app/models/commitment';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ParticipantService } from 'src/app/services/participant.service';
 
 @Component({
     selector : 'part-commitments',
     templateUrl : '../../views/commitment/participant-commitments.html',
-    providers : [CommitmentService]
+    providers : [CommitmentService, ParticipantService]
 })
 export class ParticipantCommitmentsComponent {
 
     private title : string;
     public participant : Participant;
-    public commitments : Array<Commitment>
+    public commitments : Array<Commitment>;
+    public act_id : number;
 
     constructor (
         private commitmentService : CommitmentService,
+        private participantService : ParticipantService,
         private generalProvider : GeneralProvider,
-        private _router : Router
+        private _router : Router,
+        private activatedRouter : ActivatedRoute
     ) {
-        this.participant = <Participant> this.generalProvider.getData().pop();
+
+        this.activatedRouter.paramMap.subscribe( param => {
+            this.act_id = +param.get("act_id");
+            const part_id = +param.get("part_id");
+            if (part_id == null || this.act_id == null) {
+                console.log("Incorrect argument name")
+                return;
+            }
+            
+            this.participantService.getParticipant(part_id).subscribe(
+                res => this.participant = res,
+                error => console.log("Error getting participant"),
+                () => this.loadCommitments()
+            );
+        });
+    }
+
+    public loadCommitments (): void {
         this.title = this.participant.name + "'s commitments";
         this.commitmentService.getCommitmentsByParticipantId(
             this.participant.participant_id).
             subscribe(
                 partCommit => this.commitments = partCommit, 
                 error => console.log("Error getting part commitments: ", error)
-            )
-    }
-
-    public loadDataIntoGeneralProvider(
-        url : string, 
-        commitment : Commitment
-    ) : void
-    {
-        this.generalProvider.clearData();
-        this.generalProvider.setData([commitment]);
-        this._router.navigate([url]);
+            );
     }
 
 }
