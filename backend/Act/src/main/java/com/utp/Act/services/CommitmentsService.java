cc2404d77769b0ced1cdc35db4d07f4eb58a0387
cjgc <chjugomez@utp.edu.co>
2019-08-02 17:47:26 -0500

    getCommitmentByActAndParticipantID method was implemented

--- backend/Act/src/main/java/com/utp/Act/services/CommitmentsService.java
+++ backend/Act/src/main/java/com/utp/Act/services/CommitmentsService.java
@@ -9,42 +9,41 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CommitmentsService {
-    
+
     @Autowired
     private CommitmentsRepository commitmentsRepository;
-    
-    public List<Commitments> getAllCommitments()
-    {
+
+    public List<Commitments> getAllCommitments() {
         List<Commitments> commitments = new ArrayList<>();
         commitmentsRepository.findAll().
-        forEach(commitments::add);
+                forEach(commitments::add);
         return commitments;
     }
-    
-    public Commitments getCommitment(Integer commitment_id)
-    {
+
+    public Commitments getCommitment(Integer commitment_id) {
         return commitmentsRepository.findById(commitment_id).get();
     }
-    
-    public void addCommitment(Commitments commitment)
-    {
+
+    public void addCommitment(Commitments commitment) {
         commitmentsRepository.save(commitment);
     }
-    
-    public void updateCommitment(Commitments commitment)
-    {
+
+    public void updateCommitment(Commitments commitment) {
         commitmentsRepository.save(commitment);
     }
-    
-    public void deleteCommitment(Integer id)
-    {
+
+    public void deleteCommitment(Integer id) {
         commitmentsRepository.deleteById(id);
     }
-    
-    public List<Commitments> getCommitmentsByPartcicipant(Integer 
-            participant_id) 
-    {
+
+    public List<Commitments> getCommitmentsByPartcicipant(Integer participant_id) {
         return commitmentsRepository.findByParticipantParticipantId(
                 participant_id);
+    }
+
+    public List<Commitments> getCommitmentByActAndParticipantID(
+            Integer act_id, Integer part_id) {
+        return commitmentsRepository.findByActActIdAndParticipantParticipantId(
+                act_id, part_id);
     }
 }
