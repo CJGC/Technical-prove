4e487d0cdb202b81969527741c9a5f2a905d2578
cjgc <chjugomez@utp.edu.co>
2019-08-02 17:50:36 -0500

    Method findByActActIdAndParticipantParticipantId method was implemented

--- backend/Act/src/main/java/com/utp/Act/repositories/CommitmentsRepository.java
+++ backend/Act/src/main/java/com/utp/Act/repositories/CommitmentsRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface CommitmentsRepository extends CrudRepository<Commitments, 
-        Integer>
-{
+public interface CommitmentsRepository extends CrudRepository<Commitments, Integer> {
+
     public List<Commitments> findByParticipantParticipantId(
-            Integer participant_id);
+            Integer participantId);
+
+    public List<Commitments> findByActActIdAndParticipantParticipantId(
+            Integer actId, Integer participantId);
 }
