0a7c29266ceb4e896466beb7dda91c76d6f11497
cjgc <chjugomez@utp.edu.co>
2019-08-02 17:36:12 -0500

    Print component was added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { TableModule } from 'primeng/table';
 import { DataViewModule } from 'primeng/dataview';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { PrintComponent } from './components/print/print.component';
 
 @NgModule({
   declarations: [
@@ -60,7 +61,8 @@ import { MessageService } from 'primeng/api';
     CommitmentDeleteComponent,
     ParticipantCommitmentsComponent,
 
-    MissinWebPageComponent
+    MissinWebPageComponent,
+    PrintComponent
   ],
   imports: [
     BrowserModule,
